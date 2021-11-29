e334093f85f708234015b4b719705c30d49d3f96
Luan Thome <38923629+luanripax@users.noreply.github.com>
2021-11-29 14:53:11 -0300

    theme structure initialized

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components'
 import {
   useFonts,
   Poppins_400Regular,
@@ -8,12 +10,26 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AppRoutes } from './src/routes/app.routes';
+import theme from './src/styles/theme';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </ThemeProvider>
+    
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+export default {
+    colors: {
+        dark: {
+            background_primary: '#2c2f33',
+            background_secondary: '#23272a',
+
+            select: '#7289da',
+            success: '#03B252',
+            failure: 'red',
+
+
+        },
+
+        light: {
+
+        }
+
+    },
+
+    fonts: {
+        primary_400: 'Poppins_400Regular',
+        primary_500: 'Poppins_500Medium'
+    }
+}

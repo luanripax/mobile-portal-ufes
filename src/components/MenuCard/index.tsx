2bee8454bbeb59607af6d1fc3330f9708f870d92
Luan Thome <luan_ripax@hotmail.com>
2022-03-26 16:40:01 -0300

    Fixed Type Error

--- src/components/MenuCard/index.tsx
+++ src/components/MenuCard/index.tsx
@@ -13,19 +13,9 @@ import {
 import RUImages from '../../assets/index';
 
 interface Props {
-  label: string;
-  placeholder: string;
-  value: string;
-  error: string;
-  handleChange: (e: string | ChangeEvent<any>) => void;
 }
 
 export function MenuCard({
-  label,
-  placeholder,
-  value,
-  error,
-  handleChange
 }: Props) {
 
   return (
